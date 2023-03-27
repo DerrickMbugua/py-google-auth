4c66d57ec4576f436f50dce162afae02d829ad4f
“derrick” <derrickmbugua50@gmail.com>
2023-03-27 13:16:14 +0300

    feat: google oauth

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import os
+import pathlib
+
+import requests
 from flask import Flask, session, abort, redirect, request, render_template
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
 
 app = Flask(__name__)
 app.secret_key = "mylongsecretkey"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "645914911327-epd4ka27bhcsbc219553244eh3bqqg2t.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="https://3451-41-80-113-21.ngrok.io/callback"
+)
+
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
@@ -18,12 +39,33 @@ def hello_world():
   
 @app.route("/login")
 def login():
-    session["google_id"] = "Test"
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
     return redirect("/dashboard")
   
 @app.route("/callback")
 def callback():
-    return "<p>Callback!</p>"
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    # return id_info
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/dashboard")
   
 @app.route("/logout")
 def logout():
